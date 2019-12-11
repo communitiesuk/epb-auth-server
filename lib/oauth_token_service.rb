b278146d1f5a5da44ce7aa1b6100a483358c0048
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2019-12-11 14:10:10 +0000

    Return an expiry for the token in the response to post /oauth/token

--- lib/oauth_token_service.rb
+++ lib/oauth_token_service.rb
@@ -6,7 +6,8 @@ class OAuthTokenService < Sinatra::Base
   post '' do
     status 200
     {
-      access_token: 'token'
+      access_token: 'token',
+      expires_at: Time.now.to_i + 3600
     }.to_json
   end
 end
