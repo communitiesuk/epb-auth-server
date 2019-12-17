0a92be25d1303c9ce2514692393729ca9281ee1f
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2019-12-17 13:08:46 +0000

    Add manifest generation for GoVPaaS deployment

---
+++ scripts/generate-paas-manifest.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#define parameters which are passed in.
+APPLICATION_NAME=$1  # e.g. mhclg-epb-something-api-integration
+STAGE=$2 # i.e. [integration, staging, production]
+
+cat << EOF
+---
+applications:
+  - name: $APPLICATION_NAME
+    memory: 1G
+    buildpacks:
+      - ruby_buildpack
+    health-check-type: http
+    health-check-http-endpoint: /healthcheck
+    services:
+      - logit-ssl-drain
+
+EOF
