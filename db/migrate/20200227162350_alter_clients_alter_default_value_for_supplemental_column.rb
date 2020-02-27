0042d603657ad595fa489a24356cdd5408520701
Yusuf <yusuf@madetech.com>
2020-02-27 16:35:01 +0000

    Add migration to change default value for supplemental column in clients table

---
+++ db/migrate/20200227162350_alter_clients_alter_default_value_for_supplemental_column.rb
@@ -0,0 +1,5 @@
+class AlterClientsAlterDefaultValueForSupplementalColumn < ActiveRecord::Migration[6.0]
+  def change
+    change_column_default :clients, :supplemental, from: '{}', to: {}
+  end
+end
