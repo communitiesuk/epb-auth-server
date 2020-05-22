c293fc76e8002db425ce365b65aa03b3a48cd33a
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2020-05-22 17:06:04 +0100

    Adds rotate a client secret use case

---
+++ lib/use_case/rotate_a_client_secret.rb
@@ -0,0 +1,19 @@
+module UseCase
+  class RotateAClientSecret
+    class ClientNotFoundError < StandardError; end
+
+    def initialize(container)
+      @container = container
+    end
+
+    def execute(client_id)
+      client = @container.client_gateway.fetch id: client_id
+
+      raise ClientNotFoundError unless client
+
+      client.secret = @container.client_gateway.create_secret client
+
+      client
+    end
+  end
+end
