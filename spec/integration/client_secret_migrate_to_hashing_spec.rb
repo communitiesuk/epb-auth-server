33d55a9d22bce22171618658c866905486cfe0a3
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2020-05-26 08:56:52 +0100

    Add temporary test to check migration to hashes will work

---
+++ spec/integration/client_secret_migrate_to_hashing_spec.rb
@@ -0,0 +1,34 @@
+describe "Integration: Migrating client secrets to use hashes" do
+  before(:context) do
+    Rake::Task["db:drop"].invoke
+    Rake::Task["db:create"].invoke
+    system("rake db:migrate VERSION=20200519113852")
+    ActiveRecord::Base.connection.execute(
+      "INSERT INTO clients (name, secret)
+       VALUES ('test-client', 'test-secret')",
+    )
+    system("rake db:migrate")
+  end
+  context "an existing client in the database" do
+    describe "migrating from plaintext to hashed secrets" do
+      let(:migrated_client) { Gateway::ClientGateway.new.fetch name: "test-client" }
+      let(:response) { request_token migrated_client.id, "test-secret" }
+
+      it "gives a status of 200" do
+        expect(response.status).to eq 200
+      end
+
+      it "gives a response with a valid jwt based access_token" do
+        expect(response.get(%i[access_token])).to be_a_valid_jwt_token
+      end
+
+      it "gives a response with a token that expires at least an hour in the future" do
+        expect(response.get(%i[expires_in])).to be >= 3_600
+      end
+
+      it "gives a response with a token of type Bearer" do
+        expect(response.get(%i[token_type])).to eq "bearer"
+      end
+    end
+  end
+end
