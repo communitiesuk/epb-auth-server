1d10fd0827e4bb1d4057d717125ae5d8a627f680
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2022-03-24 19:10:23 +0000

    Remove debug output now blacklist is working in integration

--- config/rack_attack_config.rb
+++ config/rack_attack_config.rb
@@ -22,27 +22,9 @@ class Rack::Attack::Request < ::Rack::Request
   end
 end
 
-# Whitelist IP addresses that ignore throttling limit; format of the env var is
-# "[{"reason":"pen testers", "ip_address": "198.51.100.111"},{...}]"
-# white_listed_ips = JSON.parse(
-#   ENV["WHITELISTED_IP_ADDRESSES"] || "[]",
-#   ).map { |item|
-#   item["ip_address"]
-# }.flatten.uniq
-#
-# Rack::Attack.safelist do |req|
-#   white_listed_ips.include? req.source_ip
-# end
-
-
-# Throttle requests to any endpoint if we receive more than X requests per min
-# Rack::Attack.throttle("Requests Rate Limit", limit: 100, period: 1.minutes, &:source_ip)
-
 # Block permanently banned IP addresses; the format of the env var is
 # "[{"reason":"did a bad thing", "ip_address": "198.51.100.100"},{...}]"
 banned_ips = JSON.parse(ENV["PERMANENTLY_BANNED_IP_ADDRESSES"] || "[]").map { |entry| entry["ip_address"] }.flatten.compact
-pp "AUTH DEBUGGING BANNED IPS: #{banned_ips}"
 Rack::Attack.blocklist("permanently blocked") do |req|
-  pp "AUTH DEBUGGING CHECKING #{req.source_ip} IS IN #{banned_ips}"
   banned_ips.include? req.source_ip
 end
