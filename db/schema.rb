6f1626455c092247ceed0fe0264c296bfa9fef83
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2025-06-17 12:11:13 +0100

    Update Ruby dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2022_02_04_112635) do
+ActiveRecord::Schema[8.0].define(version: 2022_02_04_112635) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
-  enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
   create_table "client_scopes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
