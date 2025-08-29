211a07578fe6df2225e2baa0824484254da207ad
Alice Lui <54395483+al01010@users.noreply.github.com>
2025-08-29 14:04:32 +0100

    Update rubocop yml file and run make format
    To make the change from require to plugins and remove the warning when running make format

--- db/migrate/20200227162350_alter_clients_alter_default_value_for_supplemental_column.rb
+++ db/migrate/20200227162350_alter_clients_alter_default_value_for_supplemental_column.rb
@@ -1,6 +1,4 @@
-class AlterClientsAlterDefaultValueForSupplementalColumn < ActiveRecord::Migration[
-  6.0
-]
+class AlterClientsAlterDefaultValueForSupplementalColumn < ActiveRecord::Migration[6.0]
   def change
     change_column_default :clients, :supplemental, from: "{}", to: {}
   end
